7791c63815d2b04706572d046f9bbaf8af6dc1e5
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-03-22 06:54:48 +0900

    🎉: initial commit

---
+++ src/menuItems.js
@@ -0,0 +1,52 @@
+export const menuItems = [
+  {
+    title: 'Home',
+  },
+  {
+    title: 'Services',
+    submenu: [
+      {
+        title: 'web design',
+      },
+      {
+        title: 'web development',
+        submenu: [
+          {
+            title: 'Frontend',
+          },
+          {
+            title: 'Backend',
+            submenu: [
+              {
+                title: 'NodeJS',
+              },
+              {
+                title: 'PHP',
+              },
+              {
+                title: 'Python',
+              },
+              {
+                title: 'Ruby',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: 'SEO',
+      },
+    ],
+  },
+  {
+    title: 'About',
+    submenu: [
+      {
+        title: 'Who we are',
+      },
+      {
+        title: 'Our values',
+      },
+    ],
+  },
+];
